fdbf2504e7e4846d37fb72008b835c8930a6129d
smvigneshkumar <46100885+smvigneshkumar@users.noreply.github.com>
2022-03-12 11:41:20 +0530

    code tweaking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import "./styles.css";
 import User from "./User";
 
@@ -10,24 +10,27 @@ export default function App() {
   //     .then((response) => response.json())
   //     .then((data) => setUsers(data));
   // }, []);
-  useEffect(() => {
-    async function myFn() {
-      setError(null);
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        if (!response.ok) {
-          throw new Error("Something went wrong");
-        }
-        const data = await response.json();
-        setUsers(data);
-      } catch (err) {
-        setError(err.message);
+
+  const fetchusers = useCallback(async () => {
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
       }
+      const data = await response.json();
+      setUsers(data);
+    } catch (err) {
+      setError(err.message);
     }
-    myFn();
   }, []);
+
+  useEffect(() => {
+    fetchusers();
+  }, [fetchusers]);
+
   let content = "No users found";
   if (error) {
     content = error;
